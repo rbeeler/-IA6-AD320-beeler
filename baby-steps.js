dd15cf2fb85247e86d92bb9d6af32f579a0d54b1
rbeeler <ruthbeeler3@gmail.com>
2019-02-14 14:38:00 -0800

    Add files via upload

---
+++ baby-steps.js
@@ -0,0 +1,7 @@
+var sum = 0;
+var args = process.argv;
+for (i=2;i<args.length;i++){
+  sum += +args[i];
+}
+
+console.log(sum);
---
+++ my-first-IO.js
@@ -0,0 +1,6 @@
+var fs = require('fs');
+var buf = fs.readFileSync(process.argv[2]);
+var str = buf.toString();
+var strArr = str.split('\n').length - 1;
+
+console.log(strArr);
